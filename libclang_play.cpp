42a00738c9630cef6c854d14983401d5553e80dc
Dan Weatherill (OPMD lab) <daniel.weatherill@physics.ox.ac.uk>
2017-08-16 12:00:08 +0100

    initial commit

---
+++ libclang_play.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <clang-c/Index.h>
+
+
+
+using std::cout;
+using std::endl;
+
+int main(int argc, char** argv)
+{
+  cout << "libclang play..." << endl;
+  
+  
+
+
+};
